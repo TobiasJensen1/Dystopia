fa33f0992a5b14da5c75fcedb5c4f73d8bbd1b36
Your Name <you@example.com>
2019-05-05 00:54:50 +0200

    graveyard outside event complete

--- Assets/Scripts/Graveyard/PuzzleScript.cs
+++ Assets/Scripts/Graveyard/PuzzleScript.cs
@@ -7,13 +7,22 @@ public class PuzzleScript : MonoBehaviour
 
     //Trigger mini boss
     public GameObject Necro;
+
     GameObject Player;
+
+    //doors
+    public GameObject door1;
+    public GameObject door2;
+    bool isopen;
+    float door1Y;
+    float door2Y;
+
     //lever puzzle bool
     public  bool leverA;
     public  bool leverB;
     public  bool leverC;
-
     bool isactive;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +37,23 @@ public class PuzzleScript : MonoBehaviour
             StartCoroutine("Cutscene");
           
         }
+
+
+        if(Necro.GetComponent<EnemyStats>().health == 0 && !isopen)
+        {
+
+            //not opening the door smoothly (need a fix in the future ZZzzz)
+            door1Y += door1.transform.eulerAngles.y * 0.0006f * Time.deltaTime;
+            door2Y += door2.transform.eulerAngles.y * 0.0006f * Time.deltaTime;
+          
+            door1.transform.eulerAngles = new Vector3(-90, -door1Y,0);
+            door2.transform.eulerAngles = new Vector3(-90, door2Y,0);
+            if (door1.transform.eulerAngles.y >= 90f && door1.transform.eulerAngles.y >= 90f)
+            {
+                isopen = true;
+            }
+        }
+
     }
 
     IEnumerator Cutscene()
