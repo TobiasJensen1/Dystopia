2211a585ccd5787da6b36951254887f2e325fa25
Your Name <you@example.com>
2019-05-07 17:33:23 +0200

    main menu scene added

--- Assets/Scripts/Mainmenu/Sceneview.cs
+++ Assets/Scripts/Mainmenu/Sceneview.cs
@@ -6,12 +6,15 @@ public class Sceneview : MonoBehaviour
 {
     bool scene1;
     bool scene2;
-
+    bool scene3;
 
 
     public GameObject one;
     public GameObject two;
     public GameObject three;
+    public GameObject four;
+    public GameObject five;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -38,12 +41,21 @@ public class Sceneview : MonoBehaviour
         {
             transform.Rotate(transform.eulerAngles.x * 0.6f * Time.deltaTime, 0, 0);
             if(transform.eulerAngles.x >= 50f){
-                transform.position = new Vector3(one.transform.position.x, one.transform.position.y, one.transform.position.z);
-                transform.eulerAngles = new Vector3(0, 83.7f, 0);
-                scene2 = false;
-                scene1 = true;
+                transform.position = new Vector3(four.transform.position.x, four.transform.position.y, four.transform.position.z);
+                transform.eulerAngles = new Vector3(0, 270.4f, 0);
 
+                scene1 = false;
+                scene2 = false;
+                scene3 = true;
             }
+        }
+        if (scene3)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, five.transform.position, 5 * Time.deltaTime);
+        }
+        if (Vector3.Distance(transform.position, five.transform.position) < 0.05f)
+        {
+            print("jewp");
         }
     }
 
