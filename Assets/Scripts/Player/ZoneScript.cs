720472e6e722ff21100ff851c544f7dbac972b0a
saidkayed <36620432+saidkayed@users.noreply.github.com>
2019-05-30 14:48:08 +0200

    d

---
+++ Assets/Scripts/Player/ZoneScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZoneScript : MonoBehaviour
+{
+    bool isHabour;
+    public Text text;
+
+
+     void Start()
+    {
+        text.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            if (!isHabour)
+            {
+
+                text.GetComponent<Text>().text = "Blossom Valley";
+                isHabour = true;
+                StartCoroutine("textanimation");
+            }
+            else
+            {
+                StartCoroutine("textanimation");
+                text.GetComponent<Text>().text = "Port Sarim";
+                isHabour = false;
+            }
+        }
+    }
+
+    IEnumerator textanimation()
+    {
+        text.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        text.gameObject.SetActive(false);
+        StopCoroutine("textanimation");
+    }
+     
+}
